4fe07f9375dc91a7098256070f31758ea1addfed
guilhermelima18 <guilhermelima18@hotmail.com>
2021-08-02 15:08:32 -0300

    Desafio finalizado

--- src/components/BoxTopProducts/index.js
+++ src/components/BoxTopProducts/index.js
@@ -1,39 +1,74 @@
 import React from 'react';
+import { GlobalContext } from '../../contexts/GlobalContext';
+
+import itensVendidos from '../../itensVendidos';
 
 import styles from './styles.module.scss';
 
 const BoxTopProducts = () => {
+  const { convertCurrency } = React.useContext(GlobalContext);
+
+  const totalCamisetaStarLabs = itensVendidos
+    .filter(({ nome }) => nome === "Camiseta Star Labs Preta")
+    .reduce(getTotal, 0)
+
+  const totalTenisStark = itensVendidos
+    .filter(({ nome }) => nome === "Tênis Stark Runner")
+    .reduce(getTotal, 0)
+
+  const totalTenisLuthor = itensVendidos
+    .filter(({ nome }) => nome === "Tênis Luthor Confort")
+    .reduce(getTotal, 0)
+
+  const totalBoneAcme = itensVendidos
+    .filter(({ nome }) => nome === "Boné Acme Co.")
+    .reduce(getTotal, 0)
+
+  const totalJaquetaOscorp = itensVendidos
+    .filter(({ nome }) => nome === "Jaqueta Oscorp Industries")
+    .reduce(getTotal, 0)
+
+  //Somar itens vendidos afim de obter o total
+  function getTotal(total, item) {
+    return total + (item.valorUnitario * item.quantidade);
+  };
+
   return (
     <div className={styles.cards}>
       <div className={styles.card}>
         <h1>1º</h1>
-        <h3>Camiseta Star Labs</h3>
+        <h3>Camiseta Star Labs Preta</h3>
         <span>Vendido <strong>6</strong> vezes</span>
-        <span>Resultou em <strong>R$ 190,00</strong></span>
+        <span>Preço Unitário: R$ 190,00</span>
+        <span>Total de <strong>{convertCurrency(totalCamisetaStarLabs)}</strong></span>
       </div>
-      <div className={styles.card} style={{backgroundColor: 'var(--blue-100)'}}>
+      <div className={styles.card} style={{ backgroundColor: 'var(--blue-100)' }}>
         <h1>2º</h1>
         <h3>Tênis Stark Runner</h3>
         <span>Vendido <strong>6</strong> vezes</span>
-        <span>Resultou em <strong>R$ 120,00</strong></span>
+        <span>Preço Unitário: R$ 120,50</span>
+        <span>Total de <strong>{convertCurrency(totalTenisStark)}</strong></span>
       </div>
       <div className={styles.card}>
         <h1>3º</h1>
         <h3>Tênis Luthor Confort</h3>
         <span>Vendido <strong>5</strong> vezes</span>
-        <span>Resultou em <strong>R$ 150,20</strong></span>
+        <span>Preço Unitário: R$ 150,20</span>
+        <span>Total de <strong>{convertCurrency(totalTenisLuthor)}</strong></span>
       </div>
-      <div className={styles.card} style={{backgroundColor: 'var(--gray-100)'}}>
+      <div className={styles.card} style={{ backgroundColor: 'var(--gray-100)' }}>
         <h1>4º</h1>
         <h3>Boné Acme Co.</h3>
         <span>Vendido <strong>3</strong> vezes</span>
-        <span>Resultou em <strong>R$ 99,00</strong></span>
+        <span>Preço Unitário: R$ 99,00</span>
+        <span>Total de <strong>{convertCurrency(totalBoneAcme)}</strong></span>
       </div>
-      <div className={styles.card} style={{backgroundColor: 'var(--gray-100)'}}>
+      <div className={styles.card} style={{ backgroundColor: 'var(--gray-100)' }}>
         <h1>5º</h1>
         <h3>Jaqueta Oscorp Industries</h3>
         <span>Vendido <strong>3</strong> vezes</span>
-        <span>Resultou em <strong>R$ 220,00</strong></span>
+        <span>Preço Unitário: R$ 220,00</span>
+        <span>Total de <strong>{convertCurrency(totalJaquetaOscorp)}</strong></span>
       </div>
     </div>
   );
---
+++ src/itensVendidos.js
@@ -0,0 +1,158 @@
+const itens = [
+  {
+    "id": "3ed07192-6815-4b77-b3dc-b7bdb5f24b47",
+    "idProduto": "c8bddecb-f3b2-4b4c-b655-ae1c1d732762",
+    "nome": "Tênis Stark Runner",
+    "quantidade": 1,
+    "valorUnitario": 120.50
+  },
+  {
+    "id": "57ee2448-26d2-4172-a23d-95835b8c724e",
+    "idProduto": "23280712-21e6-426a-a7fc-ee95be9b7b69",
+    "nome": "Tênis Luthor Confort",
+    "quantidade": 1,
+    "valorUnitario": 150.20
+  },
+  {
+    "id": "a7a84baa-6493-4be7-9ecd-6b256f0fd5be",
+    "idProduto": "c8bddecb-f3b2-4b4c-b655-ae1c1d732762",
+    "nome": "Tênis Stark Runner",
+    "quantidade": 1,
+    "valorUnitario": 120.50
+  },
+  {
+    "id": "25413144-7701-4717-9438-1c741a60e5a9",
+    "idProduto": "59f93be7-27b1-4ad6-bcc8-328dd65f5777",
+    "nome": "Boné Acme Co.",
+    "quantidade": 1,
+    "valorUnitario": 99.00
+  },
+  {
+    "id": "c02c8bbb-da80-467f-ad8a-63b4de262512",
+    "idProduto": "88259961-8ca0-4399-af7f-b3b8fed4a80b",
+    "nome": "Jaqueta Oscorp Industries",
+    "quantidade": 2,
+    "valorUnitario": 220
+  },
+  {
+    "id": "d724fc57-9f56-4577-aa14-0ba4196e3d20",
+    "idProduto": "1651961c-59e0-4294-ab67-7702710267fa",
+    "nome": "Agasalho Queen Industries c/ capuz",
+    "quantidade": 1,
+    "valorUnitario": 290
+  },
+  {
+    "id": "7c3a709b-43f7-489c-97b8-405a27e75548",
+    "idProduto": "c8bddecb-f3b2-4b4c-b655-ae1c1d732762",
+    "nome": "Tênis Stark Runner",
+    "quantidade": 2,
+    "valorUnitario": 120.50
+  },
+  {
+    "id": "b1f8148e-f8bc-4af4-aece-6f5b880b661c",
+    "idProduto": "68f03782-78c5-4f53-8863-13adde2d3156",
+    "nome": "Camiseta Wayne Industries",
+    "quantidade": 1,
+    "valorUnitario": 230
+  },
+  {
+    "id": "c4942317-60ab-4488-b057-c00bea52c4cb",
+    "idProduto": "3626bbf3-7f05-4e9b-9019-ed3193125be2",
+    "nome": "Camiseta Star Labs Preta",
+    "quantidade": 2,
+    "valorUnitario": 190
+  },
+  {
+    "id": "bff807f6-5afa-4903-8773-937333d9b204",
+    "idProduto": "995fc463-a179-4b07-ba45-2c4d284c6b24",
+    "nome": "Agasalho S.H.I.E.L.D.",
+    "quantidade": 1,
+    "valorUnitario": 295
+  },
+  {
+    "id": "a7ae0176-17bf-4cf9-9012-3c1d332b7567",
+    "idProduto": "995fc463-a179-4b07-ba45-2c4d284c6b24",
+    "nome": "Agasalho S.H.I.E.L.D.",
+    "quantidade": 1,
+    "valorUnitario": 295
+  },
+  {
+    "id": "e6dc6b27-caab-4c0d-a510-e0bfb702f385",
+    "idProduto": "8d9d9311-edfa-4c9b-9f4e-40a8a810d703",
+    "nome": "Boné Luthor Corp. Aba Reta",
+    "quantidade": 1,
+    "valorUnitario": 99.90
+  },
+  {
+    "id": "e727d426-e25f-410a-b6a2-44302bfbeaf4",
+    "idProduto": "3626bbf3-7f05-4e9b-9019-ed3193125be2",
+    "nome": "Camiseta Star Labs Preta",
+    "quantidade": 1,
+    "valorUnitario": 190
+  },
+  {
+    "id": "419773e6-49e2-4f6b-828c-997a101cacb7",
+    "idProduto": "3626bbf3-7f05-4e9b-9019-ed3193125be2",
+    "nome": "Camiseta Star Labs Preta",
+    "quantidade": 3,
+    "valorUnitario": 190
+  },
+  {
+    "id": "d9fd1f2d-c1f5-4e1d-9a4c-59a21c3082a8",
+    "idProduto": "8d9d9311-edfa-4c9b-9f4e-40a8a810d703",
+    "nome": "Boné Luthor Corp. Aba Reta",
+    "quantidade": 1,
+    "valorUnitario": 99.90
+  },
+  {
+    "id": "c559a575-8735-445b-b263-5a4061fd8fad",
+    "idProduto": "c8bddecb-f3b2-4b4c-b655-ae1c1d732762",
+    "nome": "Tênis Stark Runner",
+    "quantidade": 2,
+    "valorUnitario": 120.50
+  },
+  {
+    "id": "b8a18c46-a3db-4680-8232-313e12b972b5",
+    "idProduto": "23280712-21e6-426a-a7fc-ee95be9b7b69",
+    "nome": "Tênis Luthor Confort",
+    "quantidade": 2,
+    "valorUnitario": 150.20
+  },
+  {
+    "id": "3149b61c-9196-4f93-bc72-bba01384fda3",
+    "idProduto": "a33482f6-dcec-4685-9648-a3294cd8bf53",
+    "nome": "Jaqueta Umbrella Corp.",
+    "quantidade": 2,
+    "valorUnitario": 250
+  },
+  {
+    "id": "a9a550e7-4620-487f-ac05-ef8d74a64727",
+    "idProduto": "88259961-8ca0-4399-af7f-b3b8fed4a80b",
+    "nome": "Jaqueta Oscorp Industries",
+    "quantidade": 1,
+    "valorUnitario": 220
+  },
+  {
+    "id": "9c7f3848-b983-4eb9-826a-1c50feccc34c",
+    "idProduto": "59f93be7-27b1-4ad6-bcc8-328dd65f5777",
+    "nome": "Boné Acme Co.",
+    "quantidade": 2,
+    "valorUnitario": 99.00
+  },
+  {
+    "id": "d9838850-0695-42ea-9e8d-0471008ea293",
+    "idProduto": "23280712-21e6-426a-a7fc-ee95be9b7b69",
+    "nome": "Tênis Luthor Confort",
+    "quantidade": 2,
+    "valorUnitario": 150.20
+  },
+  {
+    "id": "b115facd-559d-450f-9c8d-9d6daf8d7b9d",
+    "idProduto": "1651961c-59e0-4294-ab67-7702710267fa",
+    "nome": "Agasalho Queen Industries c/ capuz",
+    "quantidade": 2,
+    "valorUnitario": 290
+  }
+];
+
+export default itens;
